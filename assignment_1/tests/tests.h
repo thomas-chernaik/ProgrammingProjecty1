d87d17586dda73d1ec6377c8c607efe2b885eaf2
Thomas Chernaik <sc21trc@leeds.ac.uk>
2022-03-01 18:23:45 +0000

    written header file for test  script

---
+++ assignment_1/tests/tests.h
@@ -0,0 +1,13 @@
+void printResults(char* function, int passed, int failed);
+void testHeaders();
+void testRead();
+void testWrite();
+void testBinWrite();
+void testCompare();
+void testReadBin();
+void testa2b();
+void testb2a();
+void testReduceSize();
+void testSubImage();
+void testInsert();
+void testEmpty();
