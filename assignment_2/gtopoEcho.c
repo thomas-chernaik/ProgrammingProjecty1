9482a5885564cc8428f7076416498d66e3622f8d
Thomas Chernaik <sc21trc@leeds.ac.uk>
2022-05-03 16:42:45 +0100

    written gtopoEcho, untested

---
+++ assignment_2/gtopoEcho.c
@@ -0,0 +1,27 @@
+/*FILNAME: gtopoEcho.c
+ * DESCRIPTION:
+ * 	clone a dem file into another file
+ * 	with comprehension of the file structure
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "gtopoRead.h"
+#include "gtopoWrite.h"
+
+int main(int argc, char **argv){
+	//validate the argument count
+	if(argc == 1){
+		printf("Usage: ./gtopoEcho inputFile width height outputFile");
+		return 0;
+	}
+	if(argc != 5){
+		printf("ERROR: Bad Argument Count\n");
+		return 1;
+	}
+	short*  file = readFile(argv[1], argv[2], argv[3]);
+	writeFile(argv[4], file, argv[2], argv[3]);
+	free(file);
+	printf("ECHOED\n");
+	return 0;
+}
