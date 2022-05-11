88ca21375658b0ce4d2cfde9d2b0b5f5f3509fbb
Thomas Chernaik <sc21trc@leeds.ac.uk>
2022-05-11 14:09:37 +0100

    modularised assemble further

--- assignment_2/gtopoInsert.c
+++ assignment_2/gtopoInsert.c
@@ -1,10 +1,13 @@
 /*FILEAME: gtopoInsert.c
  *FUNCTIONS:
  *	insert
+ *	assemble
  */
 #include <stdio.h>
 #include <stdlib.h>
 #include "gtopoInsert.h"
+#include "gtopoRead.h"
+#include "gtopoEmpty.h"
 //takes an image and an image to Insert and a start row and column and inserts the image.
 void insert(short** imageToAdd, short** image, int row, int column, int width, int height, int insertHeight, int insertWidth){
 	//goes through the image where the image needs to be inserted
@@ -15,3 +18,29 @@ void insert(short** imageToAdd, short** image, int row, int column, int width, i
 		}
 	}
 }
+short** assemble(char* filename, int width, int height, char** argv, int argc, int offset)
+        {
+        //create the image to paste the other images onto
+        short** imageData = createEmpty(width, height);
+        //now we have to loop through all the different images to put on it
+        for(int i=offset; i < (argc - offset)/5; i++)
+                {
+                //get the row number and header number from the argv
+                int row = atoi(argv[i*5+1]);
+                int col = atoi(argv[i*5+2]);
+                //get the format string from the argv
+                char* fileNamei = argv[i*5+3];
+                int imWidth = atoi(argv[i*5+4]);
+                int imHeight = atoi(argv[i*5+5]);
+                short** fileToInsert;
+                fileToInsert = readFile(fileNamei, imWidth, imHeight);
+                //add the file data to the big file in the right place
+                insert(fileToInsert, imageData, row, col, width, height, width, height);
+                //free memory
+                free(fileToInsert[0]);
+                free(fileToInsert);
+                free(fileNamei);
+                }
+        return imageData;
+        }
+
--- assignment_2/gtopoInsert.h
+++ assignment_2/gtopoInsert.h
@@ -1,1 +1,2 @@
 void insert(short** imageToAdd, short** image, int row, int column, int width, int height, int insertHeight, int insertWidth);
+short** assemble(char* filename, int width, int height, char** argv, int argc, int offset);
